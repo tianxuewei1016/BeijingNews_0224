5a926b54351faa670a5b1dabac0a68fb06b6f4a2
tianxuewei1016 <93226539@qq.com>
2017-06-01 21:01:40 +0800

    第一次提交代码

---
+++ app/src/main/java/com/atguigu/beijingnews_0224/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.atguigu.beijingnews_0224;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
