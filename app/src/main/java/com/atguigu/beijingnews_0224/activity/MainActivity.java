e55eb7f9abe054a6ad0e501954a86db5df34e1e5
tianxuewei1016 <93226539@qq.com>
2017-06-02 14:55:23 +0800

    参照slidingMenu的使用步骤做主页面

--- app/src/main/java/com/atguigu/beijingnews_0224/activity/MainActivity.java
+++ app/src/main/java/com/atguigu/beijingnews_0224/activity/MainActivity.java
@@ -1,15 +1,33 @@
 package com.atguigu.beijingnews_0224.activity;
 
+
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.atguigu.beijingnews_0224.R;
+import com.slidingmenu.lib.SlidingMenu;
+import com.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //设置主页面
         setContentView(R.layout.activity_main);
+        //设置左侧菜单
+        setBehindContentView(R.layout.left_menu);
+
+        //3.设置右侧菜单
+        SlidingMenu slidingMenu = getSlidingMenu();
+//        slidingMenu.setSecondaryMenu(R.layout.left_menu);
+
+        //设置滑动模式:左侧+主页;左侧+主页+右侧;主页+右侧
+        slidingMenu.setMode(SlidingMenu.LEFT);
+
+        //设置滑动模式:不可以滑动;滑动边缘;全屏滑动
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+        //设置主页面占的宽度
+        slidingMenu.setBehindOffset(200);
     }
 }
